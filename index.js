11dffefdab7ad38cf253aaa331883ca097b4a775
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2024-05-16 17:14:10 -0300

    Creacion del servidor y algunas rutas de prueba - arreglo de tipado en nombre de archivo

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("My App");
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.get('/', (req, res) => {
+	  res.send(`Hola mi server en express!!!`)
+})
+
+app.get('/nueva-ruta', (req, res) => {
+	  res.send(`Hola soy una nueva ruta!!!`)
+})
+
+app.get('/products', (req, res) => {
+	  res.json({
+        name: 'Product 1',
+        price: 1000
+      })
+})
+
+app.listen(port, () => {
+    console.log(`Mi puerto es: ${port}`);
+})
