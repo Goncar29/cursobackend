da348c8e1092151708e9c038b2c276779722b936
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2024-05-30 17:30:37 -0300

    Creacion metodo patch y delete para products

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -37,4 +37,22 @@ router.post('/', (req, res) => {
   })
 })
 
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: "Update",
+    data: body,
+    id
+  })
+})
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: "Update",
+    id
+  })
+})
+
 module.exports = router;
