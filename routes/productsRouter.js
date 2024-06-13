9df74b1d4be03357cb5de45535767a06546cfd47
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2024-06-12 22:50:21 -0300

    captura de errores

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -25,10 +25,17 @@ router.post('/', async (req, res) => {
 })
 
 router.patch('/:id', async (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  const product = await service.update(id, body)
-  res.json(product);
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const product = await service.update(id, body)
+    res.json(product);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    })
+  }
+
 })
 
 router.delete('/:id', async (req, res) => {
