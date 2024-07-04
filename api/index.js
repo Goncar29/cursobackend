afae0c87a9ae6f85f52ad2eb5d4eb31bc8aeaae3
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2024-07-04 15:59:30 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -7,8 +7,8 @@ const {
 	boomErrorHandler,
 } = require('./middlewares/error.handler');
 
-const port = 4000;
 const app = express();
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 
@@ -25,11 +25,11 @@ const options = {
 
 app.use(cors(options));
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
 	res.send(`Hola mi server en express!!!`);
 });
 
-app.get('/nueva-ruta', (req, res) => {
+app.get('/api/nueva-ruta', (req, res) => {
 	res.send(`Hola soy una nueva ruta!!!`);
 });
 
