fa9195fea97dd9492c4dd942a889f94acea760d9
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2024-07-05 16:03:49 -0300

    Se crear la ruta completa para categoria

--- api/routes/categoriesRouter.js
+++ api/routes/categoriesRouter.js
@@ -16,12 +16,53 @@ router.get('/', async (req, res, next) => {
 	}
 })
 
-router.get('/:id', (req, res) => {
-	const { categoryId, productId } = req.params;
-	res.json({
-		categoryId,
-		productId,
-	});
+router.get('/:id',
+	validatorHandlers(getCategorySchema, 'params'),
+	async (req, res, next) => {
+		try {
+			const { id } = req.params;
+			const category = await service.findOne(id);
+		} catch (error) {
+			next(error);
+		}
+});
+
+router.post('/:id',
+	validatorHandlers(createCategorySchema, 'body'),
+	async (req, res, next) => {
+		try {
+			const body = req.body;
+			const newCategory = await service.create(body);
+			res.status(201).json(newCategory)
+		} catch (error) {
+			next(error);
+		}
+});
+
+router.patch('/:id',
+	validatorHandlers(getCategorySchema, 'params'),
+	validatorHandlers(updateCategorySchema, 'body'),
+	async (req, res, next) => {
+		try {
+			const { id } = req.params;
+			const body = req.body;
+			const category = await service.update(id, body);
+			res.json(category)
+		} catch (error) {
+			next(error);
+		}
+});
+
+router.delete('/:id',
+	validatorHandlers(getCategorySchema, 'params'),
+	async (req, res, next) => {
+		try {
+			const { id } = req.params;
+			await service.delete(id);
+			res.status(201).json({id})
+		} catch (error) {
+			next(error);
+		}
 });
 
 module.exports = router;
