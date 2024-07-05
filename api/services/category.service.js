22c57781eb8ff009c6b3fe4df0c3ef6f2228bfe6
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2024-07-05 14:52:31 -0300

    Creacion de servicios para categorias

---
+++ api/services/category.service.js
@@ -0,0 +1,30 @@
+const { da } = require('@faker-js/faker');
+const boom = require('boom');
+
+class CategoryService {
+
+	constructor() {
+	}
+	async create(data) {
+		return data;
+	}
+
+	async find() {
+		return [];
+	}
+
+	async findOne(id) {
+		return { id };
+	}
+	async update(id, changes) {
+		return {
+			id,
+			changes,
+		};
+	}
+	async delete(id) {
+		return { id };
+	}
+}
+
+module.exports = CategoryService;
