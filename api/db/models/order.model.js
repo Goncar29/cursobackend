ab6a449e6d55d1ecb81a108f9a3d742f374e66b9
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2024-07-29 15:09:41 -0300

    Relacion muchos a muchos, order-product

--- api/db/models/order.model.js
+++ api/db/models/order.model.js
@@ -34,6 +34,12 @@ class Order extends Model {
 		this.belongsTo(models.Customer, {
 			as: 'customer',
 		});
+		this.belongsToMany(models.Product, {
+			as: 'items',
+			through: models.OrderProduct,
+			foreignKey: 'orderId',
+			otherKey: 'productId'
+		});
 	}
 
 	static config(sequelize) {
