20506eec1f313a6d631b226b2293e5633ac415b6
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2024-08-21 16:04:47 -0300

    Hashing de contraseñas con bcryptjs

---
+++ pass-veriy.js
@@ -0,0 +1,10 @@
+const bcrypt = require('bcrypt');
+
+async function verifyPassword() {
+	const myPassword = 'admin 123 .202';
+	const hash = '$2b$10$EBrN9Y4.maMP3uAT4/V1POId2SayWT5MoQzqc6u.WDNhJc58KzXIK'
+	const isMatch = await bcrypt.compare(myPassword, hash);
+	console.log(isMatch);
+}
+
+verifyPassword();
